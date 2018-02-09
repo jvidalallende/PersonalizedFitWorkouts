861c3311d6071d9167e32be5c18a1816c607fa5f
s.culebras1 <s.culebras@alumnos.urjc.es>
2018-02-09 11:36:50 +0100

    user and trainer html updated

--- src/main/java/velascogculebras/personalizedfitworkouts/Controllers/LoggingEntrenador.java
+++ src/main/java/velascogculebras/personalizedfitworkouts/Controllers/LoggingEntrenador.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import velascogculebras.personalizedfitworkouts.Repositories.EntrenadorRepository;
 
 @Controller
@@ -12,7 +13,11 @@ public class LoggingEntrenador {
     private EntrenadorRepository entrenadorRepository;
 
     @RequestMapping("/entrenador")
-    private String LoggingEntrenador(Model model){
+    private String LoggingEntrenador(Model model, @RequestParam String nombreE, @RequestParam String passwordE, @RequestParam String emailE,@RequestParam String bioE){
+        model.addAttribute("nombre",nombreE);
+        model.addAttribute("mail",emailE);
+        model.addAttribute("password",passwordE);
+        model.addAttribute("biografia",bioE);
         entrenadorRepository.findByMailAndPassword("pepe@gmail.com","pepe");
         return "Entrenador";
     }
--- src/main/java/velascogculebras/personalizedfitworkouts/Controllers/LoggingUusario.java
+++ src/main/java/velascogculebras/personalizedfitworkouts/Controllers/LoggingUusario.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import velascogculebras.personalizedfitworkouts.Repositories.UsuarioReporsitory;
 
 @Controller
@@ -12,7 +13,10 @@ public class LoggingUusario {
     private UsuarioReporsitory usuarioReporsitory;
 
     @RequestMapping("/user")
-    private String LoggingUsuario(Model model){
+    private String LoggingUsuario(Model model, @RequestParam String nombreU,@RequestParam String passwordU,@RequestParam String emailU){
+        model.addAttribute("name",nombreU);
+        model.addAttribute("mail",emailU);
+        model.addAttribute("password",passwordU);
         usuarioReporsitory.findByMailAndPassword("p@gmail.com","p");
         return "Usuario";
     }
