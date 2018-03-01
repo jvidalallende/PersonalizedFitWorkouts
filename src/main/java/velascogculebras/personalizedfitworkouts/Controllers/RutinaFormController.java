10c8abb26a6941c9c834009dafdaf4c85c7fdc5d
ivelascog1 <i.velascog@alumnos.urjc.es>
2018-03-01 12:41:12 +0100

    Added rutine repaired

--- src/main/java/velascogculebras/personalizedfitworkouts/Controllers/RutinaFormController.java
+++ src/main/java/velascogculebras/personalizedfitworkouts/Controllers/RutinaFormController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import velascogculebras.personalizedfitworkouts.Entities.Categoria;
 import velascogculebras.personalizedfitworkouts.Entities.Ejercicio;
@@ -24,31 +25,31 @@ public class RutinaFormController {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    @GetMapping("/rutinaForm")
-    public String getForm(Model model, @RequestParam String name, @RequestParam String e1, @RequestParam int s1, @RequestParam String r1,
-                          @RequestParam String e2, @RequestParam int s2, @RequestParam String r2,
-                          @RequestParam String e3, @RequestParam int s3, @RequestParam String r3,
-                          @RequestParam String e4, @RequestParam int s4, @RequestParam String r4,
-                          @RequestParam String e5, @RequestParam int s5, @RequestParam String r5,
+    @PostMapping("/rutinaForm")
+    public String getForm(Model model, @RequestParam String name, @RequestParam String e1, @RequestParam Integer s1, @RequestParam String r1,
+                          @RequestParam String e2, @RequestParam Integer s2, @RequestParam String r2,
+                          @RequestParam String e3, @RequestParam Integer s3, @RequestParam String r3,
+                          @RequestParam String e4, @RequestParam Integer s4, @RequestParam String r4,
+                          @RequestParam String e5, @RequestParam Integer s5, @RequestParam String r5,
                           @RequestParam String desc,@RequestParam String cat, HttpSession session){
         List<Ejercicio> ejercicios = new ArrayList<>();
-        if(e1 != null){
+        if (e1 != null && !e1.equals("")) {
             Ejercicio ej1 = new Ejercicio(e1,s1,r1);
             ejercicios.add(ej1);
         }
-        if(e2 != null){
+        if (e2 != null && !e2.equals("")) {
             Ejercicio ej2 = new Ejercicio(e2,s2,r2);
             ejercicios.add(ej2);
         }
-        if(e3 != null){
+        if (e3 != null && !e3.equals("")) {
             Ejercicio ej3 = new Ejercicio(e3,s3,r3);
             ejercicios.add(ej3);
         }
-        if(e4 != null){
+        if (e4 != null && !e4.equals("")) {
             Ejercicio ej4 = new Ejercicio(e4,s4,r4);
             ejercicios.add(ej4);
         }
-        if(e5 != null){
+        if (e5 != null && !e5.equals("")) {
             Ejercicio ej5 = new Ejercicio(e5,s5,r5);
             ejercicios.add(ej5);
         }
