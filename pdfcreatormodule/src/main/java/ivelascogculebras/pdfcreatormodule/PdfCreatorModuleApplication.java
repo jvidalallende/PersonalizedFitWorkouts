30905533bbac8d81a100d428ac5b5bd0f63180ff
ivelascog1 <i.velascog@alumnos.urjc.es>
2018-03-06 13:24:46 +0100

    Added internal service module

---
+++ pdfcreatormodule/src/main/java/ivelascogculebras/pdfcreatormodule/PdfCreatorModuleApplication.java
@@ -0,0 +1,12 @@
+package ivelascogculebras.pdfcreatormodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfCreatorModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfCreatorModuleApplication.class, args);
+	}
+}
---
+++ pdfcreatormodule/src/test/java/ivelascogculebras/pdfcreatormodule/PdfCreatorModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package ivelascogculebras.pdfcreatormodule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdfCreatorModuleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
