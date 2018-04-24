17a107220d1ad4cb77a8be0cf188a6e40deaa33b
ivelascog <i.velascog@alumnos.urjc.es>
2018-04-24 13:05:45 +0200

    Session replication repaired

--- src/main/java/velascogculebras/personalizedfitworkouts/Configuration/HazelcastConfiguration.java
+++ src/main/java/velascogculebras/personalizedfitworkouts/Configuration/HazelcastConfiguration.java
@@ -1,74 +1,45 @@
 package velascogculebras.personalizedfitworkouts.Configuration;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MapAttributeConfig;
+import com.hazelcast.config.MapIndexConfig;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.web.WebFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.hazelcast.HazelcastSessionRepository;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 import java.util.Arrays;
-import java.util.Properties;
 
 
+@EnableHazelcastHttpSession
 @Configuration
 public class HazelcastConfiguration {
 
-    private String[] nodes = {"192.168.33.11", "192.168.33.12"};
-
-    /**
-     * Create a Hazelcast {@code Config} object as a bean. Spring Boot will use
-     * the presence of this to determine that a {@code HazelcastInstance} should
-     * be created with this configuration.
-     * <p>
-     * As a simple side-step to possible networking issues, turn off multicast
-     * in favour of TCP connection to the local host.
-     *
-     * @return Configuration for the Hazelcast instance
-     */
     @Bean
-    public Config config() {
+    public HazelcastInstance hazelcastInstance() {
+        MapAttributeConfig attributeConfig = new MapAttributeConfig()
+                .setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+                .setExtractor(PrincipalNameExtractor.class.getName());
 
         Config config = new Config();
-
         JoinConfig joinConfig = config.getNetworkConfig().getJoin();
 
         joinConfig.getMulticastConfig().setEnabled(false);
-        joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Arrays.asList(nodes));
+        joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Arrays.asList("192.168.33.11"));
 
-        return config;
+        config.getMapConfig(HazelcastSessionRepository.DEFAULT_SESSION_MAP_NAME)
+                .addMapAttributeConfig(attributeConfig)
+                .addMapIndexConfig(new MapIndexConfig(
+                        HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false));
+
+        return Hazelcast.newHazelcastInstance(config);
     }
 
-    /**
-     * Create a web filter. Parameterize this with two properties,
-     * <p>
-     * <ol>
-     * <li><i>instance-name</i>
-     * Direct the web filter to use the existing Hazelcast instance rather than
-     * to create a new one.</li>
-     * <li><i>sticky-session</i>
-     * As the HTTP session will be accessed from multiple processes, deactivate
-     * the optimization that assumes each user's traffic is routed to the same
-     * process for that user.</li>
-     * </ol>
-     * <p>
-     * Spring will assume dispatcher types of {@code FORWARD}, {@code INCLUDE}
-     * and {@code REQUEST}, and a context pattern of "{@code /*}".
-     *
-     * @param hazelcastInstance Created by Spring
-     * @return The web filter for Tomcat
-     */
-    @Bean
-    public WebFilter webFilter(HazelcastInstance hazelcastInstance) {
-
-        Properties properties = new Properties();
-        properties.put("instance-name", hazelcastInstance.getName());
-        properties.put("sticky-session", "false");
-
-        return new WebFilter(properties);
-    }
 }
+
+
