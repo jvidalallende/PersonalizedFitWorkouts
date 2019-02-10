7acdb2cca60d49111b6d9f18e7946abc353b91d8
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-02-10 20:31:32 +0100

    Remove @PostConstruct initialization code
    This code will try to create some objects on the database upon
    application initialization. This can create trouble if more than one
    instance is run, or if the objects are left in the DB and then the
    instance is restarted.

--- main_web_app/src/main/java/velascogculebras/personalizedfitworkouts/Controllers/PrincipalController.java
+++ main_web_app/src/main/java/velascogculebras/personalizedfitworkouts/Controllers/PrincipalController.java
@@ -36,69 +36,69 @@ public class PrincipalController {
     @PostConstruct
     public void init() {
 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        List<Ejercicio> ejercicios = new ArrayList<>(4);
-        Ejercicio ejercicio = new Ejercicio("Press Militar", 4, "12-10-10-8");
-        ejercicios.add(ejercicio);
-        ejercicio = new Ejercicio("Sentadilla", 4, "10-10-8-6");
-        ejercicios.add(ejercicio);
-        Rutina rutina = new Rutina(ejercicios);
-        rutina.setNombre("rutina de prueba");
-        rutina.setDate(new Date());
-        Categoria categoria = new Categoria();
-        categoria.setNombre("CategoriaPrueba");
-        List<Rutina> rutinas = new ArrayList<>();
-        rutinas.add(rutina);
-        categoria.setRutinas(rutinas);
-        categoriaRepository.save(categoria);
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(categoria);
-        rutina.setCategorias(categorias);
-        rutina.setDescripcion("rutina de prueba para poder desarollar el front con datos");
-        rutinaRepository.save(rutina);
+        //BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        //List<Ejercicio> ejercicios = new ArrayList<>(4);
+        //Ejercicio ejercicio = new Ejercicio("Press Militar", 4, "12-10-10-8");
+        //ejercicios.add(ejercicio);
+        //ejercicio = new Ejercicio("Sentadilla", 4, "10-10-8-6");
+        //ejercicios.add(ejercicio);
+        //Rutina rutina = new Rutina(ejercicios);
+        //rutina.setNombre("rutina de prueba");
+        //rutina.setDate(new Date());
+        //Categoria categoria = new Categoria();
+        //categoria.setNombre("CategoriaPrueba");
+        //List<Rutina> rutinas = new ArrayList<>();
+        //rutinas.add(rutina);
+        //categoria.setRutinas(rutinas);
+        //categoriaRepository.save(categoria);
+        //List<Categoria> categorias = new ArrayList<>();
+        //categorias.add(categoria);
+        //rutina.setCategorias(categorias);
+        //rutina.setDescripcion("rutina de prueba para poder desarollar el front con datos");
+        //rutinaRepository.save(rutina);
 
-        Entrenador entrenador = new Entrenador();
-        entrenador.setMail("lol");
-        entrenador.setPasswordHash(encoder.encode("lol"));
-        entrenador.setName("Entrenador");
-        entrenador.setProfileIcon("/trainers/images/1.jpg");
-        entrenador.setBiografia("Soy burgalés, pero afincado en Madrid desde hace muchos años. Soy periodista y amante " +
-                "del deporte y de todo lo que le rodea. Siempre me ha gustado investigar al respecto y aprender nuevas ");
-        entrenadorRepository.save(entrenador);
+        //Entrenador entrenador = new Entrenador();
+        //entrenador.setMail("lol");
+        //entrenador.setPasswordHash(encoder.encode("lol"));
+        //entrenador.setName("Entrenador");
+        //entrenador.setProfileIcon("/trainers/images/1.jpg");
+        //entrenador.setBiografia("Soy burgalés, pero afincado en Madrid desde hace muchos años. Soy periodista y amante " +
+        //        "del deporte y de todo lo que le rodea. Siempre me ha gustado investigar al respecto y aprender nuevas ");
+        //entrenadorRepository.save(entrenador);
 
 
-        Ejercicio ejercicio1 = new Ejercicio("Zancadas", 4, "10-10-8-6f");
-        Ejercicio ejercicio2 = new Ejercicio("Peso Muerto", 5, "5-5-5-5-5f");
-        List<Ejercicio> ejercicios1 = new ArrayList<>();
-        ejercicios1.add(ejercicio1);
-        ejercicios1.add(ejercicio2);
-        Rutina rutina2 = new Rutina(ejercicios1);
-        rutina2.setNombre("Test2");
-        rutina2.setDescripcion("Rutina para probar el for");
-        rutina2.setEntrenador(entrenador);
-        rutina2.setDate(new Date());
-        entrenador.setRutinas(rutinas);
-        rutinaRepository.save(rutina2);
+        //Ejercicio ejercicio1 = new Ejercicio("Zancadas", 4, "10-10-8-6f");
+        //Ejercicio ejercicio2 = new Ejercicio("Peso Muerto", 5, "5-5-5-5-5f");
+        //List<Ejercicio> ejercicios1 = new ArrayList<>();
+        //ejercicios1.add(ejercicio1);
+        //ejercicios1.add(ejercicio2);
+        //Rutina rutina2 = new Rutina(ejercicios1);
+        //rutina2.setNombre("Test2");
+        //rutina2.setDescripcion("Rutina para probar el for");
+        //rutina2.setEntrenador(entrenador);
+        //rutina2.setDate(new Date());
+        //entrenador.setRutinas(rutinas);
+        //rutinaRepository.save(rutina2);
 
-        Usuario usuario1 = new Usuario();
-        usuario1.setMail("p@gmail.com");
-        usuario1.setName("Jose");
-        usuario1.setPasswordHash(encoder.encode("p"));
-        usuarioReporsitory.save(usuario1);
+        //Usuario usuario1 = new Usuario();
+        //usuario1.setMail("p@gmail.com");
+        //usuario1.setName("Jose");
+        //usuario1.setPasswordHash(encoder.encode("p"));
+        //usuarioReporsitory.save(usuario1);
 
-        Entrenador entrenador1 = new Entrenador();
-        entrenador1.setName("pepe");
-        entrenador1.setPasswordHash(encoder.encode("pepe"));
-        entrenador1.setMail("pepe@gmail.com");
+        //Entrenador entrenador1 = new Entrenador();
+        //entrenador1.setName("pepe");
+        //entrenador1.setPasswordHash(encoder.encode("pepe"));
+        //entrenador1.setMail("pepe@gmail.com");
 
-        entrenadorRepository.save(entrenador1);
+        //entrenadorRepository.save(entrenador1);
 
-        Comentario comentario = new Comentario();
-        comentario.setComentario("De Puta madre niño");
-        comentario.setDate(new Date());
-        comentario.setUser(usuario1);
-        comentario.setRutina(rutina2);
-        comentarioRepository.save(comentario);
+        //Comentario comentario = new Comentario();
+        //comentario.setComentario("De Puta madre niño");
+        //comentario.setDate(new Date());
+        //comentario.setUser(usuario1);
+        //comentario.setRutina(rutina2);
+        //comentarioRepository.save(comentario);
     }
 
     @RequestMapping("/")
