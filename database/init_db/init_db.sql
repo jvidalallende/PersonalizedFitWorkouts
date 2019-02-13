75f05bac40758f09d621fa735f2ae5db2fb35837
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-02-13 18:53:48 +0100

    Use separate permissions for main web app and pdfcreator
     - Main web app can read and write
     - PDFcreator can only read

--- database/init_db/init_db.sql
+++ database/init_db/init_db.sql
@@ -1,3 +1,9 @@
+-- Database
 CREATE DATABASE test;
+
+-- Main web app can read and write
 CREATE USER 'server' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON test.* TO 'server';
+
+-- PDF creator module only has read permissions
+GRANT SELECT ON test.* TO 'pdfcreator'@'%' IDENTIFIED BY 'pdfcreator-password';
