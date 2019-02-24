3f0001212db211ee63f9fdc0d57578822bc813b0
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-02-24 16:02:52 +0100

    Add eval minikube docker-env to build_containers.bash

--- scripts/build_containers.bash
+++ scripts/build_containers.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+eval $(minikube docker-env)
 docker rmi juanvidal-pfw-app juanvidal-pdfcreator
 docker build -t juanvidal-pfw-app ../pfw_app/
 docker build -t juanvidal-pdfcreator ../pdfcreatormodule/
