fba1d4087733b83b2e1d030737de78bb58dc8d22
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-02-24 17:09:01 +0100

    Improve building script, and add option to change image name in helm

--- scripts/build-pfw-containers.bash
+++ scripts/build-pfw-containers.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Get the path to scripts folder based on this script's path
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PFW_ROOT="$(dirname "$SCRIPTS_DIR")"
+
 PFW_MAIN="juanvidal-pfw-main"
 PDF_CREATOR="juanvidal-pdfcreator"
 
 eval $(minikube docker-env)
-docker rmi ${PFW_MAIN} ${PDF_CREATOR}
-docker build -t ${PFW_MAIN} ../pfw_main/
-docker build -t ${PDF_CREATOR} ../pdfcreator/
+docker build -t ${PFW_MAIN} ${PFW_ROOT}/pfw_main/
+docker build -t ${PDF_CREATOR} ${PFW_ROOT}/pdfcreator/
